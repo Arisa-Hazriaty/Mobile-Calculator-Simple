f8b69e6baab63b1f8aa68cd27d575a1be485b8e7
ArisaHazriaty <Arisa.hazriaty@bhinneka.com>
2021-08-07 21:23:26 +0700

    Commit Pertama Mobile-Calculator

---
+++ Object Repository/Button - 1.rs
@@ -0,0 +1,18 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<MobileElementEntity>
+   <description></description>
+   <name>Button - 1</name>
+   <tag></tag>
+   <elementGuidId>60cf0bf2-1e2a-4e50-b4e7-0f5d8c1e5fa7</elementGuidId>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>resource-id</name>
+      <type>Main</type>
+      <value>com.google.android.calculator:id/digit_1</value>
+   </webElementProperties>
+   <locator>//*[@resource-id = 'com.google.android.calculator:id/digit_1']</locator>
+   <locatorStrategy>ATTRIBUTES</locatorStrategy>
+</MobileElementEntity>
---
+++ Test Suites/Mobile.ts
@@ -0,0 +1,44 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Mobile</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>314531d0-79c5-4bd8-a98c-58129930ef3b</testSuiteGuid>
+   <testCaseLink>
+      <guid>29dc3ee4-250f-491d-8d8b-28fd90f6bc31</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/0.StartApplication</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>fa7f228b-72bf-4f66-bff0-2b5113217638</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/1.TestCase-Perkalian</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>321526e6-e62a-4720-948c-b8e403f5fb23</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/2.TestCase-Penjumlahan</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>401fb361-a984-4ec9-9f34-256719a91678</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/3.TestCase-Pengurangan</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>5ae090e3-6c0a-4b50-bf15-42880eda91d4</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/4.TestCase-Pembagian</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
